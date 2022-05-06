99693821b708ca57a349c0139bb11e565091397a
kalmykov-a <kalmykoff93@mail.ru>
2022-05-06 15:22:21 +0300

    3 tasks for homework #2

--- task02-slice.go
+++ task02-slice.go
@@ -2,8 +2,8 @@ package homework
 
 func reverse(input []int64) (result []int64) {
 	//Place your code here
-	for i, v := range input {
-		result[len(input)-1+i] = v
+	for i, _ := range input {
+		result = append(result, input[len(input)-1])
 	}
 	return
 }
