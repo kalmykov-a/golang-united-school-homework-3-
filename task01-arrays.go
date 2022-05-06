ddbb627f6338a787347bb6e8f7e0f9b0fdc9f014
kalmykov-a <kalmykoff93@mail.ru>
2022-05-06 15:15:51 +0300

    3 tasks for homework

--- task01-arrays.go
+++ task01-arrays.go
@@ -2,5 +2,9 @@ package homework
 
 func average(input [15]float32) (result float32) {
 	//Place your code here
+	for _, v := range input {
+		result += v
+	}
+	result /= float32(len(input))
 	return
 }
--- task03-map.go
+++ task03-map.go
@@ -1,6 +1,20 @@
 package homework
 
+import "sort"
+
 func sortMapValues(input map[int]string) (result []string) {
 	//Place your code here
+	var keys []int
+
+	for k, _ := range input {
+		keys = append(keys, k)
+	}
+
+	sort.Ints(keys)
+
+	for _, v := range keys {
+		result = append(result, input[v])
+	}
+
 	return
 }
